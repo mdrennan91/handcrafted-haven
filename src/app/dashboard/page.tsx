63b20f635ed35dbaa00fe0b7bab74b342576eabd
Kerri Morris <kkmorrisfam@hotmail.com>
2025-05-31 10:09:35 -0700

    Add temporary page.tsx page back in

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Artisan Admin Dashboard',
+};
+
+
+export default function Page() {
+    return <p>Artisan Admin Page</p>
+}
