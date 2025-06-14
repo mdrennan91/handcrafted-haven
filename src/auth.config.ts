28b0e7f757ff67af71a635e5b7dd9e3679a6ad04
Kerri Morris <kkmorrisfam@hotmail.com>
2025-06-14 09:21:05 -0700

    change auth structure

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,6 +1,6 @@
 import type { NextAuthConfig } from 'next-auth';
 
-//******need to change path to seller's dashboard when that is figured out.***********/
+
 export const authConfig = {
   secret: process.env.AUTH_SECRET,
   pages: {
@@ -21,17 +21,37 @@ export const authConfig = {
       }
       return session;
     },
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard'); //could use homepage, or whatever page needs to be logged in to view
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+    // authorized({ auth, request: { nextUrl } }) {
+    //   const isLoggedIn = !!auth?.user;
+    //   const isOnDashboard = nextUrl.pathname.startsWith('/dashboard'); //could use homepage, or whatever page needs to be logged in to view
+    //   if (isOnDashboard) {
+    //     if (isLoggedIn) return true;
+    //     return false; // Redirect unauthenticated users to login page
+    //   } else if (isLoggedIn) {
+    //     return Response.redirect(new URL('/dashboard', nextUrl));
+    //   }
+    //   return true;
+    // },
+    async authorized({auth, request: {nextUrl}}) {
+      const user = auth?.user;
+      const role = user?.role;     
+      const path = nextUrl.pathname;
+
+      // if path is /admin and role is admin, return true
+      // false otherwise
+      if (path.startsWith('/admin')) {
+        return role === "Admin"
       }
+
+      // if path is /dashboard and role is seller, return true
+      if(path.startsWith('/dashboard') ){
+        return role === "Seller"
+      }
+
+      //all other routes - if this is false, every request goes back to /login
       return true;
-    },
+
+    }
   },
   providers: [], // Add providers with an empty array for now
    debug: true,
